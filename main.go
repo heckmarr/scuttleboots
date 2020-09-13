e36d664c65eb10d3a0e2d27de939c7595b436a87
Ent <benbelland@gmail.com>
2020-09-12 22:02:32 -0700

    should serve a page

--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
-	""
-
-	)
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("This will be the server")
+	server := gin.Default()
+	server.GET("/screen", RenderScreen)
+
+}
+
+func RenderScreen(c *gin.Context) {
+	//Put call to python3 crunching here
+	c.String(http.StatusOK, "Whale oil beef hook")
 }
